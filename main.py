024d6de4bfa598faf12ee27a5b5c50d8ad30a1e4
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-09-10 01:17:38 +0100

    trigger action when the webcam detects an object

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 # wait for 1 second for camera to load
 time.sleep(1)
 
 first_frame = None
+status_list = []
 
 while True:
+    status = 0
     # If you have a laptop, for example you have an integrated camera
     # then you should place zero to use that main camera.
     check, frame = video.read()
@@ -38,7 +41,16 @@ while True:
             # between the static image and the current frame
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
 
     cv2.imshow("Video", frame)
 
